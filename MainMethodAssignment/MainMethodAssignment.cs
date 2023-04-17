4ca77a3f2a4096bca44f1a9ec68d6d18b422bb47
Loduye Ghaisen <lloduye@gmail.com>
2023-04-17 16:29:43 -0300

    updated

--- MainMethodAssignment/MainMethodAssignment.cs
+++ MainMethodAssignment/MainMethodAssignment.cs
@@ -9,13 +9,25 @@ namespace MainMethodAssignment
     public class MathMethods
     {
         //First method that takes in an integer
-        public int MathOp (int number)
-        { 
-            return number + 7; 
+        public int MathOp(int number)
+        {
+            return number + 7;
         }
+
         //Seccond method that takes a decimal
+        public decimal MathOp(decimal number)
+        {
             // Convert the decimal to an int to perform math operation
+            int newnum = Convert.ToInt32(number);
+            return newnum * 12;
+        }
+
         //Third mathOp method that takes a string
+        public int MathOp(string number)
+        {
             //convert the string to an int to perform math operation
+            int newnum2 = Convert.ToInt32(number);
+            return newnum2 % 3;
+        }
     }
 }
