d1dd7bb0736539c8708260949ae4695fbfe4b46b
Loduye Ghaisen <lloduye@gmail.com>
2023-04-17 15:58:25 -0300

    Updated

---
+++ MainMethodAssignment/MainMethodAssignment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathMethods
+    {
+        //First method that takes in an integer
+        public int MathOp (int number)
+        { 
+            return number + 7; 
+        }
+        //Seccond method that takes a decimal
+            // Convert the decimal to an int to perform math operation
+        //Third mathOp method that takes a string
+            //convert the string to an int to perform math operation
+    }
+}
---
+++ MainMethodAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MainMethodAssignment
+{
+    class Program 
+    {
+     static void Main(string[] args)
+        {
+            //Instantiate the mathMethod class
+            //Call the first method with an int parameter
+            //call the second method with a decimal parameter
+            // Call the third method with a string parameter
+        }
+    }
+}
