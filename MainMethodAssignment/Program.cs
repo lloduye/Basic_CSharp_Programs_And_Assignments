4a5c8876e5467c868740d0a1ad169967ca42b963
Loduye Ghaisen <lloduye@gmail.com>
2023-04-17 16:29:43 -0300

    updated

--- MainMethodAssignment/Program.cs
+++ MainMethodAssignment/Program.cs
@@ -8,9 +8,16 @@ namespace MainMethodAssignment
      static void Main(string[] args)
         {
             //Instantiate the mathMethod class
+            MathMethods math1 = new MathMethods();
+
             //Call the first method with an int parameter
+            Console.WriteLine(math1.MathOp(7));
+
             //call the second method with a decimal parameter
+            Console.WriteLine(math1.MathOp(8.3m));
+
             // Call the third method with a string parameter
+            Console.WriteLine(math1.MathOp("7"));
         }
     }
 }
