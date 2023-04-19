62bf26bdff075fa09a3fa0193028b79eba4db218
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-19 19:50:39 -0300

    New TwentyOne

---
+++ TwentyOne/Card.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+public class Card
+{
+    public string Face { set ; get; }
+    public string Suit { set ; get; }
+}
---
+++ TwentyOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card cardOne = new Card();
+            cardOne.Suit = "Queen";
+            cardOne.Face = "Spades";
+
+            Console.WriteLine("The card is " + cardOne.Suit + " of " + cardOne.Face);
+        }
+    }
+}
