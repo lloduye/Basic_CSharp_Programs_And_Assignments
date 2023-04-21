90745e40226eba1f7ff99f94d6077426c503da72
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-21 11:50:01 -0300

    update

---
+++ TwentyOne/Player.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Player
+{
+	public string Name { get; set; }
+    public List<Card> Hand { get; set;}
+    public bool IsActive { get; set; }
+    public int Balance { get; set; }
+}
