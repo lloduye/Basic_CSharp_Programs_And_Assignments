7e49944950e5bf5a1afee71bb42a23e6623c42b3
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-24 22:45:07 -0300

    Updated

--- OperatorsAssignment/Employee.cs
+++ OperatorsAssignment/Employee.cs
@@ -5,6 +5,12 @@ public class Employee
 {
     //Class properties with FirstName, LastName and Id
     public int Id { get; set; }
-    public string FirstName { get; set; }    
-    public string LastName { get; set; 
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public static bool comparisonOp (Employee a, Employee b)
+    {
+        Employee.comparisonOp (a, b);
+        return true ;
+    }
 }
--- OperatorsAssignment/Program.cs
+++ OperatorsAssignment/Program.cs
@@ -6,17 +6,31 @@ namespace OperatorsAsssignment
     class Program
     {
         static void Main()
-        {
+        {   
+
+            //Instantiating the two objects from the Emolpyee class
+            
+            //Object 1
             Employee Michael = new Employee();
             Michael.FirstName = "Michael";
             Michael.Id = 203;
 
+            //Object 2
             Employee Susan = new Employee();
             Susan.FirstName = "Susan";
             Susan.Id = 42;
 
-            Michael.Id += Susan.Id;
-            Console.ReadLine();
+            Employee.comparisonOp(Michael, Susan); 
+                {
+                    if (Michael.Id == Susan.Id)
+                    {
+                    Console.WriteLine(Michael.FirstName + " Has the same ID " + Michael.Id + " as " + Susan.Id);
+                    }
+                    else
+                    {
+                    Console.WriteLine(Michael.FirstName + " employee Id is not same as " + Susan.FirstName + " employee Id");
+                    }
+                };
         }
     }
 }
