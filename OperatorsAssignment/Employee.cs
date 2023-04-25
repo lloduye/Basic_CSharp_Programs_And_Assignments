7fa38966771ce80bf6b241a1be8ed8995f8e14f1
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-24 22:03:06 -0300

    OperatorsAssignment

---
+++ OperatorsAssignment/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+//Create an Employee class
+public class Employee
+{
+    //Class properties with FirstName, LastName and Id
+    public int Id { get; set; }
+    public string FirstName { get; set; }    
+    public string LastName { get; set; 
+}
---
+++ OperatorsAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace OperatorsAsssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee Michael = new Employee();
+            Michael.FirstName = "Michael";
+            Michael.Id = 203;
+
+            Employee Susan = new Employee();
+            Susan.FirstName = "Susan";
+            Susan.Id = 42;
+
+            Michael.Id += Susan.Id;
+            Console.ReadLine();
+        }
+    }
+}
