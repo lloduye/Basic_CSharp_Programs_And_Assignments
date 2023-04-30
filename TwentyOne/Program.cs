de2136a9705788ce782783c2e29341c24128c69a
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-29 22:17:07 -0300

    Building Black Jack game

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -9,49 +9,5 @@ namespace TwentyOne
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //instatiating a deck by creating a deck
-            Deck deck = new Deck();
 
-            //Return deck
-            deck= Shuffle(deck, 3);
-
-            //printing all the cards in the deck to console
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            //Cards count method to see how many cards are there
-            Console.WriteLine(deck.Cards.Count);
-        }
-
-        //Create a shuffle function that takes in a deck of cards,
-        //shuffles them and returns a shuffled deck
-        public static Deck Shuffle(Deck deck, int times = 1)
-        {
-            for (int i = 0; i < times; i++)
-            {
-
-            }
-            //create a temporary list of cards
-            List<Card> Templist = new List<Card>();
-            //instantite a random by creating random
-            Random random = new Random();
-
-            //Create while loop
-            while (deck.Cards.Count > 0)
-            {
-                //create a random index
-                int randomindex = random.Next(0, deck.Cards.Count);
-                //Add deck of cards to a temporary list created
-                Templist.Add(deck.Cards[randomindex]);
-                //Remove deck of cards to a temporary list created
-                deck.Cards.RemoveAt(randomindex);
-            }
-            deck.Cards = Templist;
-            return deck;
-        }
-           
     }
-}
