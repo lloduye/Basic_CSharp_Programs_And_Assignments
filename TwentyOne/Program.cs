2cc51f34fb8d72f5f8e2589e7e1dbce006e6b6e3
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-04-29 22:51:13 -0300

    updated

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -15,7 +15,13 @@ namespace TwentyOne
             string playerName = Console.Readline();
             Console.ReadLine();
             Console.WriteLine("And How much money did you bring today?");
-            Console.ReadLine();
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join the game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower;
+            if (answer == "yes" || answer=="yeah" || answer=="y" ||answer=="ya")
+            {
+                Player player = new Player();
+            }
         }
     }
 }
