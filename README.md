893d906b0bf014b0f46643966362346ed365d46c
Johnmond Ghaisen <117686102+lloduye@users.noreply.github.com>
2023-05-21 10:52:48 -0300

    Final Assignmen
    Create a basic Entity Framework Code-First console application that creates a basic Student database and adds one student.

--- README.md
+++ README.md
@@ -1,5 +1,39 @@
-# The_Tech_Academy_Basic_C-Sharp_Projects
-This repository is for all my C# and .NET Course Projects.
+# C# and .NET Framework Small Projects
 
-Be sure to check here again soon as i will be updating this 
-about everytime i work on the course projects.thank you
+This repository contains a collection of small projects I developed using C# and .NET Framework. 
+These projects showcase different applications and functionalities using various technologies within 
+the C# ecosystem. Below, you'll find a brief description of each project:
+
+## 1. Car Insurance System
+A car insurance system that mimics an insurance website. It provides functionalities to manage 
+insurance policies, calculate quotes based on user inputs, and perform various administrative tasks related to car insurance.
+
+## 2. Student Database Console Application
+A console application that allows you to register students and add them to a database. You can perform 
+operations like adding new students, retrieving student information, updating student details, and deleting student records.
+
+## 3. Twenty One (Blackjack) Console App Game
+A console application that simulates the popular card game "Twenty One" (Blackjack). It includes functionalities 
+for dealing cards, calculating scores, implementing player decisions, and determining the winner based on the game rules.
+
+## 4. Newsletter Web App
+A web application that allows users to sign up for a newsletter subscription on the website. User information, such as name 
+and email address, is saved in a database. The application provides a user-friendly interface for managing subscriptions, 
+sending newsletters, and performing administrative tasks related to the newsletter service.
+
+
+Please refer to the individual project folders for more detailed information and instructions on running each project.
+
+## Requirements
+- .NET Framework [version]
+- [Other dependencies or prerequisites]
+
+## Getting Started
+1. Clone this repository: `git clone [https://github.com/lloduye/Basic_CSharp_Programs_And_Assignments]`
+2. Navigate to the desired project folder: `cd [project folder]`
+3. [Additional steps or instructions specific to each project]
+
+Feel free to explore each project, contribute improvements, or use them as a reference for your own projects. 
+If you encounter any issues or have suggestions, please don't hesitate to open an issue or pull request.
+
+Thank you for visiting this repository! I hope you find these projects helpful and insightful.
---
+++ StudentDatabase/StudentDatabase/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+
+namespace StudentDatabase
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentContext())
+            {
+                // Create the database 
+                context.Database.CreateIfNotExists();
+
+
+                // Create a new student
+
+                Console.WriteLine("Hello, Welcome to Student Database \nWould you like to register a new student?");
+                {
+                    string response = Console.ReadLine();
+
+                    if (response == "yes" || response == "yeah" || response == "y")
+
+                    {
+                        //Collecting user input
+
+                        Console.WriteLine("What's the student's first name? ");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("What's the student's last name? ");
+                        string lastName = Console.ReadLine();
+                        Console.WriteLine("Please enter the student's age: ");
+                        int age = int.Parse(Console.ReadLine());
+
+                        //Create a new student
+                        var student = new Student
+                        {
+                            FirstName = firstName,
+                            LastName = lastName,
+                            Age = age
+                        };
+
+                        // Add the student to the database
+                        context.Students.Add(student);
+                        context.SaveChanges();
+
+                        Console.WriteLine("Student added successfully!");
+
+                        // Retrieve all students from the database
+                        var students = context.Students;
+                        Console.WriteLine("All students in the database:");
+
+                        foreach (var s in students)
+                        {
+                            Console.WriteLine($"Id: {s.Id}, First Name: {s.FirstName}, Last Name: {s.LastName}, Age: {s.Age}");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thank you for using the program! \nSee you soon");
+                    }
+                    Console.WriteLine("Press any key to exit...");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
